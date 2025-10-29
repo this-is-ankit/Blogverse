f8dab549520bbfa40c83c0949112abed97a6bfe0
Ankit Kumar <ankitmayank829@gmail.com>
2025-10-29 18:53:24 +0530

    Almost done

--- routes/blog.js
+++ routes/blog.js
@@ -1,7 +1,8 @@
 const { Router } = require("express");
 const multer = require("multer");
 const path = require("path");
-const Blog = require("../models/blog")
+const Blog = require("../models/blog");
+const Comment = require("../models/comment");
 
 const router = Router();
 
@@ -32,4 +33,21 @@ router.post("/" ,upload.single('coverImage') , async (req , res) => {
     })
     return res.redirect(`/blog/${blog._id}`);
 });
+router.get("/:id" , async (req , res) => {
+  const blog = await Blog.findById(req.params.id).populate("createdBy");
+  const comments = await Comment.find({blogId : req.params.id}).populate("createdBy");
+  res.render("blog" , {
+    user : req.user,
+    blog,
+    comments,
+  } )
+})
+router.post("/comment/:blogId", async (req , res) => {
+  await Comment.create({
+    content : req.body.content,
+    blogId : req.params.blogId,
+    createdBy : req.user._id,
+  });
+  return res.redirect(`/blog/${req.params.blogId}`);
+})
 module.exports = router
